71b4959457067025f2c5bae90ce3dbbfc3ed10b7
Kiyoshi Akila Tira <kiyoshi.atira@gmail.com>
2026-07-13 08:51:39 +0700

    update route: view() blade + tambah route informasi

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->file(resource_path('views/home-infosewa.html'));
-});
-
-Route::get('/pesan', function () {
-    return response()->file(resource_path('views/pesan-infosewa.html'));
-});
